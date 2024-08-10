4d4e19f65619ac5a5a0f67afcffca9235e13cb79
Mateus <mateusparreiras16@gmail.com>
2024-08-10 14:02:06 -0300

    Primeiro exericico

---
+++ Exercicio01/src/exercicios/Soma.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.*;
+class Soma {
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		int num1, num2, soma;
+		System.out.println("Digite um numero: ");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro numero: ");
+		num2 = sc.nextInt();
+		soma = num1 + num2;
+		System.out.println("A soma de " + num1 + " + " + num2 + " eh igual a "
+				+ soma);
+		
+	}
+
+}
