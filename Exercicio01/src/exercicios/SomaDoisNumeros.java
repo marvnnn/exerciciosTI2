c7a12885b1c129e2bb22d3e56909d823ba098569
Mateus <mateusparreiras16@gmail.com>
2024-08-10 14:16:43 -0300

    atualizar

--- Exercicio01/src/exercicios/SomaDoisNumeros.java
+++ Exercicio01/src/exercicios/SomaDoisNumeros.java
@@ -1,18 +1,15 @@
 package exercicios;
-
 import java.util.*;
-class Soma {
-	public static Scanner sc = new Scanner(System.in);
-	public static void main(String[] args) {
-		int num1, num2, soma;
-		System.out.println("Digite um numero: ");
-		num1 = sc.nextInt();
-		System.out.println("Digite outro numero: ");
-		num2 = sc.nextInt();
-		soma = num1 + num2;
-		System.out.println("A soma de " + num1 + " + " + num2 + " eh igual a "
-				+ soma);
-		
-	}
 
+class SomaDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Digite um numero: ");
+		int num1 = sc.nextInt();
+		System.out.println("Digite outro numero: ");
+		int num2 = sc.nextInt();
+		int soma = num1 + num2;
+		System.out.println("A soma eh: " + soma);
+	}
 }
